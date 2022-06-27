32bbd5ba0c84a30b7632cce6b40b7e4cdcd9a90b
sreekalyan <sreekalyan@streamsets.com>
2022-06-27 18:07:05 +0530

    This is to test the jenkis configuration

--- sample.sh
+++ sample.sh
@@ -3,7 +3,7 @@
 set -e
 
 number_of_environments=`echo $env | tr -cd , | wc -c`
-number_of_environment=`expr $number_of_environments + 1`
+number_of_environments=`expr $number_of_environments + 1`
 for i in $(seq 1 $number_of_environments)
 do
 echo $env | cut -d "," -f $i
