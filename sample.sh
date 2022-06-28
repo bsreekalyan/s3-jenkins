a9bcf89f4c412372b90b69d1e76000293255b983
sreekalyan <sreekalyan@streamsets.com>
2022-06-28 08:56:53 +0530

    This is to test the jenkis configuration

--- sample.sh
+++ sample.sh
@@ -7,5 +7,5 @@ number_of_environments=`expr $number_of_environments + 1`
 for i in $(seq 1 $number_of_environments)
 do
 echo $env | cut -d "," -f $i
-aws s3 cp --recursive "s3://sreekalyan-enterprise/release/${PROJECT}/${VERSION}-${RC_VERSION}" "s3://sreekalyan-enterprise/release/${PROJECT}/${VERSION}"
+aws s3 cp --recursive "s3://sreekalyan-enterprise/release/${PROJECTS}/${VERSION}-${RC_VERSION}" "s3://sreekalyan-enterprise/release/${PROJECTS}/${VERSION}"
 done
