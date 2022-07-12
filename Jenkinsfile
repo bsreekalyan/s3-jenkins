62ec3aadb347746b8b9b2bdc1457bbea92ed1c1e
sreekalyan <sreekalyan@streamsets.com>
2022-07-12 15:32:39 +0530

    Update the testing files

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,15 +1,27 @@
+/* groovylint-disable LineLength */
+/* groovylint-disable-next-line CompileStatic */
 pipeline {
     agent any
     parameters {
         booleanParam(name: 'RELOAD_JOB', defaultValue: false, description: 'Reload job from Jenkinsfile and exit')
-        string(name: 'PROJECTS', description: 'Enter the name of Projects that need to be deployed with a comma seperator')
+        string(name: 'PROJECTS', description: 'Enter Single/Multiple Project Names separated by comma delimiter. example: azure-data-warehouse,oracle')
         string(name: 'VERSION', defaultValue: '', description: 'Version number for the new release')
         string(name: 'RC_VERSION', defaultValue: '', description: 'Release candidate version from where to create the new version')
-        }
+    }
     stages {
-        stage('Copy the Release Candidate to final Directory in aws s3 bucket'){
-            steps{
-                withAWS(credentials: 'sam-jenkins-demo-credentials', region: 'us-west-2')
+        stage('Reload pipeline from Jenkinsfile') {
+            when {
+                expression { return params.RELOAD_JOB == true }
+            }
+            steps {
+                echo('Ended pipeline after reloading')
+            }
+        }
+        stage('Copy the Release Candidate to final Directory in aws s3 bucket') {
+            when {
+                expression { params.RELOAD_JOB == false }
+            }
+            steps {
                 sh """ #!/usr/bin/env bash
                 set -eu
                 export env=$PROJECTS
--- sample.sh
+++ sample.sh
@@ -2,10 +2,39 @@
 
 set -e
 
-number_of_environments=`echo $env | tr -cd , | wc -c`
-number_of_environments=`expr $number_of_environments + 1`
-for i in $(seq 1 $number_of_environments)
+number_of_environments=$(echo "$env" | tr -cd , | wc -c)
+number_of_environments=$((number_of_environments + 1))
+
+#Check if the Version and RC version bucket exists.
+aws s3 ls "s3://sreekalyan-enterprise-1/release/${PROJECTS}/${VERSION}-${RC_VERSION}" 2>/dev/null
+bucket_check=$?
+if [ $bucket_check == 0 ]; then
+     echo "The Version and RC Version provided are correct"
+else
+     echo "Please re-enter the correct Version and RC Version"
+     exit 1
+fi
+
+#Copy the RC candidate into New release Bucket.
+for i in $(seq 1 "$number_of_environments")
 do
-echo $env | cut -d "," -f $i
-aws s3 cp --recursive "s3://sreekalyan-enterprise/release/${PROJECTS}/${VERSION}-${RC_VERSION}" "s3://sreekalyan-enterprise/release/${PROJECTS}/${VERSION}"
+    echo "$env" | cut -d "," -f "$i"
+    aws s3 cp --recursive "s3://sreekalyan-enterprise-1/release/${PROJECTS}/${VERSION}-${RC_VERSION}" "s3://sreekalyan-enterprise-1/release/${PROJECTS}/${VERSION}"
 done
+
+# Check the copy is successful
+aws s3 ls "s3://sreekalyan-enterprise-1/release/${PROJECTS}/${VERSION}" 2>/dev/null
+copy_check=$?
+if [ $copy_check == 0 ]; then
+     count_origion=$(aws s3 ls "s3://sreekalyan-enterprise-1/release/${PROJECTS}/${VERSION}-${RC_VERSION}")
+     count_destination=$(aws s3 ls "s3://sreekalyan-enterprise-1/release/${PROJECTS}/${VERSION}")
+     if [ "$count_origion" == "$count_destination" ]; then
+        echo " The copy is successful"
+     else
+        echo " The copy is not complete, Please check the bucket contents manually"
+        exit 1
+    fi
+else
+     echo "Something went wrong ! please check bucket contents manually through UI"
+     exit 1
+fi
